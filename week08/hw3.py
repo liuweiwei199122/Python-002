a6686277f93f70198be631f047ab7675b42d07df
liuweiwei199122 <41248564+liuweiwei199122@users.noreply.github.com>
2020-09-14 04:10:52 +0800

    Update hw3.py

--- week08/hw3.py
+++ week08/hw3.py
@@ -1,14 +1,24 @@
 #实现一个 @timer 装饰器，记录函数的运行时间，注意需要考虑函数可能会接收不定长参数。
-import timeit
-def time(func,*args,**kargs):
-    #return timeit.timeit(stmt=func)
+import time
+from functools import reduce
+def timer(func):
+    def decorate(*args, **kargs):
+        start = time.process_time()
+        func(*args, **kargs)
+        ret = time.process_time()- start
+        return f'{ret: .5f}'
+    return decorate
     
-    print(timeit.timeit(stmt=func))
+@timer  
+def add(*args, **kargs):
+    t = 0
+    if len(args) == 0:
+        return 0
+    else:
+        for i in args:
+            t += i
+        return t
+    
 
-@time    
-def addone():
-    return 1
 
-
-print(addone())
-#print(timeit.timeit(stmt=[i for i in range(10)]))
+print(add(1, 3))
